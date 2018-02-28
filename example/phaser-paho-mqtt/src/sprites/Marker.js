d2fe0eb5a250f2e197ccbe899d11990e971ef3a5
Robby <ta7382@gmail.com>
2018-02-28 15:54:56 +0800

    Add marker

--- example/phaser-paho-mqtt/src/sprites/Marker.js
+++ example/phaser-paho-mqtt/src/sprites/Marker.js
@@ -9,7 +9,14 @@ export default class extends Phaser.Sprite {
     }) {
         super(game, x, y, asset)
         this.game.physics.arcade.enable(this)
+        this.game.add.existing(this)
+        this.counter = 0
     }
 
-    update() {}
+    update() {
+        this.counter += 1
+        if (this.counter > 50) {
+            this.kill()
+        }
+    }
 }
